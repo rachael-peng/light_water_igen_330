70a1d7ebee572d3985a27f9fe39c626a06130418
ruixue <rpeng7@student.ubc.ca>
2023-12-26 23:14:31 -0700

    Initial version of project.

---
+++ script.js
@@ -0,0 +1,5 @@
+document.write("This is a JS code");
+
+document.addEventListener('click', function() {
+    window.location.href = '2_IntroPage.html'; // Change the URL to the desired next page
+});
