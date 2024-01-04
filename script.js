071bd2c254389c7071f0c0e6f28b1bde74e27358
ruixue <rpeng7@student.ubc.ca>
2024-01-04 12:58:23 -0700

    Page 1 complete

--- script.js
+++
@@ -1,5 +0,0 @@
-document.write("This is a JS code");
-
-document.addEventListener('click', function() {
-    window.location.href = '2_IntroPage.html'; // Change the URL to the desired next page
-});
