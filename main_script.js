f2e646469e8906e43894db42e758ac8c5b553f7b
ruixue <rpeng7@student.ubc.ca>
2024-01-06 00:10:27 -0700

    minor changes

--- main_script.js
+++ main_script.js
@@ -4,3 +4,27 @@
 function redirectToPage(pageUrl) {
     window.location.href = pageUrl;
 }
+http://192.168.1.84/26/on
+// GET request 
+function sendGetRequest() {
+    const url = '???';
+
+    // Use jQuery library to send the GET request
+    $.ajax({ // used to send an asynchronous HTTP GET request
+      url: url,
+      type: 'GET',
+      dataType: 'json',
+      
+      success: function(data) {
+        // Handle the data from the response
+        console.log('Response:', data);
+        alert('Received Data:\n' + JSON.stringify(data, null, 2));
+      },
+
+      error: function(jqXHR, textStatus, errorThrown) {
+        // Handle errors
+        console.error('Error:', textStatus, errorThrown);
+        alert('Error: ' + textStatus + ' - ' + errorThrown);
+      }
+    });
+  }
